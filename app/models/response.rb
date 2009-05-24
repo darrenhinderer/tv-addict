1d5d73c94a42a75457adafbeed20bfe6b410f08d
Darren Hinderer <darren.hinderer@gmail.com>
2009-05-24 15:58:21 -0600

    get ignored area going with some other style changes

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,14 +3,13 @@ class Response < ActiveRecord::Base
   belongs_to :show
 
   WATCHING = "watching"
-  WATCHED = "watched"
+  WATCHED = "watched" #deprecated sorta
   NEVER_WATCH = "never watch"
 
   def self.alphabetized_responses(user_id)
     responses = find_all_by_facebook_user_id(user_id, :include => "show")
     responses.sort! { |a,b| a.show.name.downcase <=> b.show.name.downcase }
   end
-
 
   def self.find_watching_friends(friends, show)
     responses = find(:all, :conditions => {:status => WATCHING, :show_id => show.id}, :include => :facebook_user)
@@ -32,14 +31,6 @@ class Response < ActiveRecord::Base
     response.show_id = show_id
     response.facebook_user_id = user_id
     response.status = WATCHING
-    response.save
-  end
-
-  def self.create_watched_response(show_id, user_id)
-    response = Response.new
-    response.show_id = show_id
-    response.facebook_user_id = user_id
-    response.status = WATCHED
     response.save
   end
 
